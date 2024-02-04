f09678d655af9a8db6e36a054a5d9d55f8a8eeed
Chenxi <lin.chenxi14@gmail.com>
2024-02-04 15:26:02 -0500

    added hover states and onclick button function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from 'react'
-import moon from './assets/icon-moon.svg'
-import sun from './assets/icon-sun.svg'
+import Moon from './assets/icon-moon.svg?react'
+import Sun from './assets/icon-sun.svg?react'
 import search from './assets/icon-search.svg'
 import Profile from './components/Profile'
 import './App.scss'
 
 function App() {
-  const[username, setUsername] = useState('octocat')
-  const[result, setResult] = useState('')
+  const [username, setUsername] = useState('octocat')
+  const [result, setResult] = useState('')
+  const [getProfile, setGetProfile] = useState(false)
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${username}`)
+    .then(response => response.json())
+    .then(data => setProfile(data))
+  }, [])
+
   useEffect(() => {
     getUser()
-    return () => getUser()
-  }, [username, result])
+    if(getProfile) return getUserOnClick()
+  }, [username, getProfile])
 
   const[profile, setProfile] = useState({})
   const[mode, setMode] = useState('lightMode')
@@ -23,10 +31,17 @@ function App() {
         if(data.message) {
           setResult('No results')
         } else {
-          setProfile(data)
           setResult('')
         }
       })
+  }
+
+  function getUserOnClick() {
+    fetch(`https://api.github.com/users/${username}`)
+      .then(response => response.json())
+      .then(data => setProfile(data))
+
+    setGetProfile(false)
   }
 
   function lightDarkMode() {
@@ -41,7 +56,7 @@ function App() {
         <h1 className={`${mode}`}>devfinder</h1>
         <div onClick={lightDarkMode} className={`light-mode ${mode}`}>
           <p className={`${mode}`}>{mode === 'dark-mode' ? 'LIGHT' : 'DARK'}</p>
-          {mode === 'lightMode' ? <img src={moon} alt="" width={20} height={20} />: <img src={sun} alt="" /> }
+          {mode === 'lightMode' ? <Moon className={mode} alt="" width={20} height={20} /> : <Sun className={mode} alt="" width={20} height={20} /> }
         </div>
       </header>
       <section className={`${mode}-input`}>
@@ -52,7 +67,10 @@ function App() {
           </div>
           <div className='button-container'>
             {result && <p className='no-result'>{result}</p>}
-            <button onClick={getUser}>Search</button>
+            <button onClick={(e) => {
+              e.preventDefault()
+              setGetProfile(true)
+            }}>Search</button>
           </div>
         </form>
       </section>
